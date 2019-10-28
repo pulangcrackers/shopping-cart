e94bbfce6ef550f12de25380bc7e38d1e1b74acd
paulangcrackers <bmariepaula121@gmail.com>
2019-10-28 13:56:51 +0800

    Updates

---
+++ precache-manifest.3aeacf2356037a69a14c995a98d9911f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5f80b4c5718a348c399496108e38142",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "a4b4a478142870df9cb7",
+    "url": "/shopping-cart/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "2cb21fe519cd122cc3a1",
+    "url": "/shopping-cart/static/js/2.4e46b304.chunk.js"
+  },
+  {
+    "revision": "a4b4a478142870df9cb7",
+    "url": "/shopping-cart/static/js/main.01eae0ca.chunk.js"
+  },
+  {
+    "revision": "9b8b61c842c88e9d6315",
+    "url": "/shopping-cart/static/js/runtime-main.42ab9bae.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/shopping-cart/static/media/logo.25bf045c.svg"
+  }
+]);
